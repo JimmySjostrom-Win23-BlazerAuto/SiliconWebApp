081c6aad030be6f439e8e1399f9ef22864cced90
sniffmcmuffin <sjostrom.jimmy@gmail.com>
2024-05-23 09:35:48 +0200

    Updated and bug fixes.

--- SiliconWebApp/SiliconWebApp/Data/UserProfile.cs
+++ SiliconWebApp/SiliconWebApp/Data/UserProfile.cs
@@ -5,6 +5,6 @@ public class UserProfile
 	public string Id { get; set; } = Guid.NewGuid().ToString();
 	public string FirstName { get; set; } = null!;
 	public string LastName { get; set; } = null!;
-	public string? ProfilePicture { get; set; }
+	public string? ProfilePicture { get; set; } = "avatar.jpg";
 	public string? Biography { get; set; }
 }
