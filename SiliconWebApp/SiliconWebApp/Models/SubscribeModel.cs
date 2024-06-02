af3d62c450b3c7118b4c5bef365b23960e6737bc
sniffmcmuffin <sjostrom.jimmy@gmail.com>
2024-06-02 20:55:42 +0200

    Fixed most of Notifications. Still some checks that need to get in place.

---
+++ SiliconWebApp/SiliconWebApp/Models/SubscribeModel.cs
@@ -0,0 +1,12 @@
+﻿namespace SiliconWebApp.Models;
+
+public class SubscribeModel
+{
+    public string Email { get; set; } = null!;
+    public bool DailyNewsletter { get; set; }
+    public bool AdvertisingUpdates { get; set; }
+    public bool WeekinReview { get; set; }
+    public bool EventUpdates { get; set; }
+    public bool StartupsWeekly { get; set; }
+    public bool Podcasts { get; set; }
+}
